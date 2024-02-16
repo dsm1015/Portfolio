e3a3ade024d34cffc255766bae40457ffc697605
Douglas M <dsm1015@sru.edu>
2024-02-16 12:46:19 -0500

    remake portfolio to react

--- layouts/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="flex flex-col h-screen">
-        <LayoutNavbar />
-        <div class="flex-1">
-        <slot />
-        </div>
-        <LayoutFooter />
-    </div>
-</template>
